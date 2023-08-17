8840c0dd37ee62b43a0dbf021777d85170b7773c
Mommy Me <mkm0182@yahoo.com>
2023-08-17 07:36:07 -0500

    Added to the script

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,97 +95,39 @@ function init() {
 {
     q: "The condition in an if/else statement is enclosed within_____.",
     a: [{ text: "1. quotes", isCorrect: false, isSelected: false },
-    { text: "2. curly brackets", isCorrect: true },
+    { text: "2. curly brackets", isCorrect: false },
     { text: "3. parentheses", isCorrect: false },
-    { text: "4. square brackets", isCorrect: false }
+    { text: "4. square brackets", isCorrect: true }
     ]
 
 },
 {
     q: "Arrays in JavaScript can be used to store _______.",
-    a: [{ text: "numbers & strings", isCorrect: false },
-    { text: "other arrays", isCorrect: false },
-    { text: "booleans", isCorrect: false },
-    { text: "all of the above", isCorrect: true }
+    a: [{ text: "1. numbers & strings", isCorrect: false },
+    { text: "2. other arrays", isCorrect: false },
+    { text: "3. booleans", isCorrect: false },
+    { text: "4. all of the above", isCorrect: true }
     ]
 },
 {
     q: "Sting values must be enclosed within ______ when being assigned to variables?",
-    a: [{ text: "commas", isCorrect: false },
-    { text: "curly brackets", isCorrect: false },
-    { text: "quotes", isCorrect: true },
-    { text: "parentheses", isCorrect: false }
+    a: [{ text: "1. commas", isCorrect: false },
+    { text: "2. curly brackets", isCorrect: false },
+    { text: "3. quotes", isCorrect: true },
+    { text: "4. parentheses", isCorrect: false }
     ]
 },
 {
     q: "A very useful tool used during development and debugging for printing content to the debugger it:",
-        a: [{ text: "JavaScript", isCorrect: false },
-        { text: "terminal/bash", isCorrect: false },
-        { text: "for looping", isCorrect: false },
-        { text: "console log", isCorrect: true }
+        a: [{ text: "1 .JavaScript", isCorrect: false },
+        { text: "2. terminal/bash", isCorrect: false },
+        { text: "3. for looping", isCorrect: false },
+        { text: "4. console log", isCorrect: true }
         ]
 },
 
 
 ]
 
-let currQuestion = 0
-let score = 0
-
-function loadQues() {
-    const question = document.getElementById("ques")
-    const opt = document.getElementById("opt")
-
-    question.textContent = Questions[currQuestion].q;
-    opt.innerHTML = ""
-
-    for (let i = 0; i < Questions[currQuestion].a.length; i++) {
-        const choicesdiv = document.createElement("div");
-        const choice = document.createElement("input");
-        const choiceLabel = document.createElement("label");
-
-        choice.type = "radio";
-        choice.name = "answer";
-        choice.value = i;
-
-        choiceLabel.textContent = Questions[currQuestion].a[i].text;
-
-        choicesdiv.appendChild(choice);
-        choicesdiv.appendChild(choiceLabel);
-        opt.appendChild(choicesdiv);
-    }
-}
-
-loadQues();
-
-function loadScore() {
-    const totalScore = document.getElementById("score")
-    totalScore.textContent = `You scored ${score} out of ${Questions.length}`
-}
-
-
-function nextQuestion() {
-    if (currQuestion < Questions.length - 1) {
-        currQuestion++;
-        loadQues();
-    } else {
-        document.getElementById("opt").remove()
-        document.getElementById("ques").remove()
-        document.getElementById("btn").remove()
-        loadScore();
-    }
-}
-
-function checkAns() {
-    const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
-
-    if (Questions[currQuestion].a[selectedAns].isCorrect) {
-        score++;
-        console.log("Correct")
-        nextQuestion();
-    } else {
-        nextQuestion();
-    }
-}
 
   
